62225f664c4b7acb4713c4883ebf985b10b688c4
Adam F <46360510+feldsteina@users.noreply.github.com>
2019-06-27 15:25:43 -0500

    cleaned up formatting

--- application.py
+++ application.py
@@ -5,22 +5,22 @@ from flask_sqlalchemy import SQLAlchemy
 from config import (user, password, host, port, database)
 
 try:
-    connection = psycopg2.connect(user = user,
-                                password = password,
-                                host = host,
-                                port = port,
-                                database = database)
+    connection = psycopg2.connect(user=user,
+                                  password=password,
+                                  host=host,
+                                  port=port,
+                                  database=database)
     cursor = connection.cursor()
     # Print PostgreSQL Connection properties
-    print ( connection.get_dsn_parameters(),"\n")
+    print(connection.get_dsn_parameters(), "\n")
     # Print PostgreSQL version
     cursor.execute("SELECT version();")
     record = cursor.fetchone()
-    print("You are connected to - ", record,"\n")
-except (Exception, Error) as error :
-    print ("Error while connecting to PostgreSQL", error)
+    print("You are connected to - ", record, "\n")
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL", error)
 finally:
-    #closing database connection.
+    # closing database connection.
     if(connection):
         cursor.close()
         connection.close()
@@ -40,15 +40,18 @@ application.config['DEBUG'] = True
 def index():
     return render_template("index.html")
 
+
 @application.route("/movie")
 def movie():
 
     return render_template("movie.html")
 
+
 @application.route("/recommended")
 def recommended():
 
     return render_template("recommended.html")
+
 
 if __name__ == "__main__":
     application.run()
