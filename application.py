1efbaa714aebda9f075336b39ca5649687a672ec
Theodore Moreland <theodoremoreland@yahoo.com>
2021-10-03 09:12:11 -0500

    refactor: update spacing in application.py

--- application.py
+++ application.py
@@ -4,23 +4,35 @@ import random
 
 # Third party
 import pg8000
-from flask import (Flask, render_template, request, redirect, session, flash, jsonify)
+from flask import (
+    Flask
+    , render_template
+    , request
+    , redirect
+    , session
+    , flash
+    , jsonify
+)
 
 # Custom
 from scripts.model import (make_recommendation, worst_recommendations)
 from scripts.model import (model_knn, movie_matrix, movie_title_index)
-from scripts.config import (user, password, host, port, database, secret_key)
-
+from scripts.config import (
+    user
+    , password
+    , host
+    , port
+    , database
+    , secret_key
+)
 
 application = Flask(__name__)
 application.config['DEBUG'] = True
 application.secret_key = secret_key
 
 
-
 @application.before_first_request
 def setup():
-
     global cursor, id_index
 
     id_index = {"id": [], "title": []}
@@ -31,7 +43,6 @@ def setup():
                                     host=host,
                                     port=int(port),
                                     database=database)
-        
         cursor = connection.cursor()
 
         # Print PostgreSQL version
@@ -52,7 +63,6 @@ def setup():
 
 @application.route("/", methods=['POST', 'GET'])
 def index():
-
     global cursor, id_index
 
     try:
@@ -63,6 +73,7 @@ def index():
 
     except (Exception) as error:
         print ("Error while selecting rows(title, movie_id)", error)  
+
     return render_template("index.html", all_movies=all_movies)
 
 
@@ -78,24 +89,25 @@ def about():
 
 @application.route('/login', methods=['POST', 'GET'])
 def login():
-
     global cursor
 
     if request.method == 'POST':
-
         username = request.form['username']
         cursor.execute('BEGIN TRANSACTION;')
         cursor.execute(f"SELECT user_id, username, watchlist FROM users WHERE username = '{username}';") # postgres can only interpret single quoted strings
         user_info = cursor.fetchone()
         print(user_info)
+
         if user_info:
             if user_info[1] == username:
                 session['username'] = username
+
                 return redirect(f"/profile/{username}")
             else:
                 flash('invalid username')
         else:
             flash('invalid username')
+
     return render_template('login.html')
 
 
@@ -107,12 +119,12 @@ def logout():
         flash(f"{username} is now logged out")
     else:
         flash("You need to log in before you can log out")
+
     return redirect("/login")
 
 
 @application.route('/profile/<username>', methods=['POST', 'GET'])
 def render_profile(username):
-
     global cursor
 
     if 'username' in session:
@@ -125,10 +137,12 @@ def render_profile(username):
         cursor.execute(f"SELECT watchlist FROM users WHERE username = '{username}';")
         watchlist = cursor.fetchone() # Returns 2d array wherein first and singular element contains actual data.
         movieIDs = []
+
         for movie in watchlist[0]:
             cursor.execute(f"SELECT movie_id FROM movie_list WHERE title = '{movie}';")
             movieID = cursor.fetchone()[0]
             movieIDs.append(movieID)
+
         return render_template("profile.html", movieIDs=movieIDs, watchlist=watchlist, username=username)
 
     except (Exception) as error:
@@ -139,13 +153,11 @@ def render_profile(username):
 
 @application.route('/info/<movie_id>')
 def display_movie_info(movie_id):
-
     global cursor
 
     cursor.execute('BEGIN TRANSACTION;')
     cursor.execute(f"SELECT * FROM metadata WHERE id = '{movie_id}';")
     metadata = cursor.fetchone()
-
 
     _id = metadata[0]
     title = metadata[1]
@@ -163,7 +175,6 @@ def display_movie_info(movie_id):
 
 @application.route('/watchlist/<movie_id>', methods=['POST', 'GET'])
 def add_to_watchlist(movie_id):
-
     global cursor
 
     try:
@@ -178,21 +189,23 @@ def add_to_watchlist(movie_id):
         cursor.execute(f"SELECT watchlist FROM users WHERE username = '{username}';")
         recently_added_to_watchlist = cursor.fetchone()[0][-1]
         flash(f"{recently_added_to_watchlist} successfully added to watchlist!")
+
         return redirect(f"/info/{movie_id}")
         
     except (Exception) as error:
         print ("Error while querying database", error)
         flash("There was an error when attempting to add to watchlist!")
+
         return redirect(f"/info/{movie_id}")
 
 
 @application.route('/remove/<movie>', methods=['POST', 'GET'])
 def remove_from_watchlist(movie):
-
         global cursor
 
         movie = movie.replace(",", ",,")
         movie = movie.replace("'", "''")
+
         try:
             username = session["username"]
             cursor.execute('BEGIN TRANSACTION;')
@@ -200,12 +213,12 @@ def remove_from_watchlist(movie):
             cursor.execute('COMMIT;')
         except (Exception) as error:
             print ("Error while querying database", error)
+
         return redirect(f'/profile/{username}')
 
 
 @application.route('/choices', methods=['POST', 'GET'])
 def recommend_positive_movies():
-
     global cursor, id_index
 
     #(title, dist)
@@ -218,30 +231,30 @@ def recommend_positive_movies():
         print(movies)
 
         recommendations1.append(
-        make_recommendation(
-            model_knn=model_knn,
-            data=movie_matrix,
-            fav_movie = movies[0],
-            mapper=movie_title_index,
-            n_recommendations=10)
+            make_recommendation(
+                model_knn=model_knn,
+                data=movie_matrix,
+                fav_movie = movies[0],
+                mapper=movie_title_index,
+                n_recommendations=10)
         )
 
         recommendations2.append(
-        make_recommendation(
-            model_knn=model_knn,
-            data=movie_matrix,
-            fav_movie = movies[1],
-            mapper=movie_title_index,
-            n_recommendations=10)
+            make_recommendation(
+                model_knn=model_knn,
+                data=movie_matrix,
+                fav_movie = movies[1],
+                mapper=movie_title_index,
+                n_recommendations=10)
         )
 
         recommendations3.append(
-        make_recommendation(
-            model_knn=model_knn,
-            data=movie_matrix,
-            fav_movie = movies[2],
-            mapper=movie_title_index,
-            n_recommendations=10)
+            make_recommendation(
+                model_knn=model_knn,
+                data=movie_matrix,
+                fav_movie = movies[2],
+                mapper=movie_title_index,
+                n_recommendations=10)
         )
 
         #titles
@@ -260,10 +273,13 @@ def recommend_positive_movies():
             recommendations = eval("recommendations" + j)
             mix = eval("mix" + j)
             dist = eval("dist" + j)
+
             for _ in range(5):
                 scramble = random.randint(0, 9)
+
                 while recommendations[0][0][scramble] in mix or recommendations[0][0][scramble] in movies:
                     scramble = random.randint(0, 9)
+
                 mix.append(recommendations[0][0][scramble])
                 dist.append(recommendations[0][1][scramble])
         
@@ -274,10 +290,12 @@ def recommend_positive_movies():
         for i in range(3):
             j = str(i + 1)
             mix = eval("mix" + j)
+
             for movie in mix:
                 movie = movie.replace(",", "").lower()
                 for i, title in enumerate(id_index["title"]):
                     title_l = title.lower()
+
                     if movie in title_l:
                         movie_id = id_index["id"][i]
                         eval("list" + j).append([title, movie_id])
@@ -296,10 +314,8 @@ def recommend_positive_movies():
 
 @application.route('/worst', methods=['POST', 'GET'])
 def recommend_negative_movies():
-
     global cursor, id_index
 
-    
     recommendations1 = []
     recommendations2 = []
     recommendations3 = []
@@ -309,30 +325,30 @@ def recommend_negative_movies():
         print(movies)
 
         recommendations1.append(
-        worst_recommendations(
-            model_knn=model_knn,
-            data=movie_matrix,
-            fav_movie = movies[0],
-            mapper=movie_title_index,
-            n_recommendations=100)
+            worst_recommendations(
+                model_knn=model_knn,
+                data=movie_matrix,
+                fav_movie = movies[0],
+                mapper=movie_title_index,
+                n_recommendations=100)
         )
 
         recommendations2.append(
-        worst_recommendations(
-            model_knn=model_knn,
-            data=movie_matrix,
-            fav_movie = movies[1],
-            mapper=movie_title_index,
-            n_recommendations=100)
+            worst_recommendations(
+                model_knn=model_knn,
+                data=movie_matrix,
+                fav_movie = movies[1],
+                mapper=movie_title_index,
+                n_recommendations=100)
         )
 
         recommendations3.append(
-        worst_recommendations(
-            model_knn=model_knn,
-            data=movie_matrix,
-            fav_movie = movies[2],
-            mapper=movie_title_index,
-            n_recommendations=100)
+            worst_recommendations(
+                model_knn=model_knn,
+                data=movie_matrix,
+                fav_movie = movies[2],
+                mapper=movie_title_index,
+                n_recommendations=100)
         )
 
         #titles
@@ -346,16 +362,20 @@ def recommend_negative_movies():
         dist3 = []
 
         print(recommendations1)
+
         #for each set of recommedations (3), pick a random recommendation and append to "mix" list
         for i in range(3):
             j = str(i + 1)
             recommendations = eval("recommendations" + j)
             mix = eval("mix" + j)
             dist = eval("dist" + j)
+
             for _ in range(5):
                 scramble = random.randint(0, 9)
+
                 while recommendations[0][0][scramble] in mix or recommendations[0][0][scramble] in movies:
                     scramble = random.randint(0, 9)
+
                 mix.append(recommendations[0][0][scramble])
                 dist.append(recommendations[0][1][scramble])
         
@@ -366,14 +386,19 @@ def recommend_negative_movies():
         for i in range(3):
             j = str(i + 1)
             mix = eval("mix" + j)
+
             for movie in mix:
                 movie = movie.replace(",", "").lower()
+
                 for i, title in enumerate(id_index["title"]):
                     title_l = title.lower()
+
                     if movie in title_l:
                         movie_id = id_index["id"][i]
                         eval("list" + j).append([title, movie_id])
+
         print(list3, dist3)
+
         return jsonify({'data': render_template('worst.html',
                                                  list1=list1, 
                                                  list2=list2,
@@ -384,7 +409,6 @@ def recommend_negative_movies():
                                                  movies=movies)})
     
     return redirect("/")
-
 
 
 if __name__ == "__main__":
