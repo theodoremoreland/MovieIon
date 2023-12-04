e3992594dc47d1c281d19a396bb0e3277630e371
theodoremoreland <theodoremoreland@yahoo.com>
2023-12-04 14:52:53 -0600

    slot machine style intro now doesn't always start upon reload

--- application/static/js/intro.js
+++ application/static/js/intro.js
@@ -1,18 +1,32 @@
-var toggleButton = document.getElementById('toggleSlider'),
-    slide = document.querySelector('.slide');
+window.onload = function () {
+  const hasSeenIntro = localStorage.getItem("hasSeenIntro");
+  var toggleButton = document.getElementById("toggleSlider");
+  var slide = document.querySelector(".slide");
 
-toggleButton.addEventListener('click', toggleSlider, false);
+  toggleButton.addEventListener("click", toggleSlider, false);
 
-function toggleSlider() {
-  if (slide.classList.contains('slide-up')) {
-    slide.classList.remove('slide-up');
-  } else {
-    slide.classList.add('slide-up');
+  function toggleSlider() {
+    if (slide.classList.contains("slide-up")) {
+      slide.classList.remove("slide-up");
+    } else {
+      slide.classList.add("slide-up");
+
+      toggleButton.removeEventListener("click", toggleSlider, false);
+      localStorage.setItem("hasSeenIntro", true);
+    }
   }
-}
 
-setTimeout(transition, 4000);
+  function transition() {
+    toggleButton.click();
+  }
 
-function transition() {
-  toggleButton.click();
-}
+  if (hasSeenIntro) {
+    toggleButton.style.display = "none";
+    slide.style.display = "none";
+  } else if (window.innerWidth < 800) {
+    toggleButton.style.display = "none";
+    slide.style.display = "none";
+  } else {
+    setTimeout(transition, 4000);
+  }
+};
